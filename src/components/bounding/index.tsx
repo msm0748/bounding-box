25eca96a1f42459dcd67474f0d232267f2201385
Moon <msm0748@gmail.com>
2023-03-22 14:21:06 +0900

    feat: Add right-click disable feature

--- src/components/bounding/index.tsx
+++ src/components/bounding/index.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Main from "./main";
 import Tutorial from "./Tutorial";
 
 function Bounding() {
-    const [isShowTutorial, setIsShowTutorial] = useState<boolean>(false);
+    const [isShowTutorial, setIsShowTutorial] = useState<boolean>(true);
     const handleTutorialToggle = () => {
         setIsShowTutorial((prevState) => !prevState);
     };
+    useEffect(() => {
+        const preventDefault = (e: MouseEvent) => {
+            e.preventDefault();
+        };
+        document.addEventListener("contextmenu", preventDefault, { passive: false });
+        return () => {
+            document.removeEventListener("contextmenu", preventDefault);
+        };
+    }, []);
     return (
         <>
             <Header handleTutorialToggle={handleTutorialToggle} />
